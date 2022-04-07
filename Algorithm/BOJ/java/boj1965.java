b1689a5971bcd57ffded7a37416760f96fc26124
김은빈 <rlaisqls@gmail.com>
2022-04-07 23:11:06 +0900

    Update boj1965.java

--- Algorithm/BOJ/java/boj1965.java
+++ Algorithm/BOJ/java/boj1965.java
@@ -14,6 +14,8 @@ public class boj1965 {
             dp[i] = 1;
             for(int j = 0; j < i; j++){
                 if(arr[j] < arr[i]) dp[i] = (dp[i] > dp[j]+1) ? dp[i] : dp[j]+1;
+                /*dp[i] = i번째 칸까지의 상자중에 가장 증가하는 부분 수열
+                j < i인동안 쭉 탐색해서 i번째 칸 숫자보다 작은 칸(j) 중에 dp[j]가 가장 큰거 +1을 dp[i]에 넣어줌*/
             }
             res = (res > dp[i]) ? res : dp[i];
         }
