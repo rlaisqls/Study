b0a399508e19569a17760dda6309c2e0d3535abc
김은빈 <rlaisqls@gmail.com>
2022-04-21 07:23:40 +0900

    Add files via upload

---
+++ backend/springboot/crud/src/main/java/com/example/crud/CrudApplication.java
@@ -0,0 +1,15 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class CrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudApplication.class, args);
+    }
+
+}
