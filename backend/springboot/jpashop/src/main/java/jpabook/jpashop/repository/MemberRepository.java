15cbfb5bb7754595992321d73a6bd7e324bb9846
rlaisqls <rlaisqls@gmail.com>
2022-05-26 12:23:19 +0900

    .

--- backend/springboot/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ backend/springboot/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -5,19 +5,26 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
+import java.util.List;
 @Repository
 public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
-
-    public Long save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
-
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
     }
-}
 
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m",Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
+}
