323ab5ff09f90acab5dec98445d3c24987395458
rlaisqls <rlaisqls@gmail.com>
2022-07-10 15:28:16 +0900

    🌱 :: 테스트 Constant class 추가

---
+++ backend/springboot/shoppingmall/src/test/java/com/practice/shoppingmall/constant/CouponConstant.java
@@ -0,0 +1,20 @@
+package com.practice.shoppingmall.constant;
+
+import java.time.LocalDateTime;
+
+public class CouponConstant {
+
+    public static final Long USER_COUPON_ID = 1L;
+
+    public static final LocalDateTime LOCAL_DATE_TIME_NOW = LocalDateTime.of(2022,1,1,1,5);
+
+    public static final LocalDateTime COUPON_EXPIRED_DATE_TIME = LocalDateTime.of(2022,1,3,0,0);
+
+    public static final Long ID = 1L;
+
+    public static final int DISCOUNT_PRICE = 1000;
+
+    public static final String NAME = "방학기념 할인";
+
+    public static final Long VALIDITY_PERIOD = 86400L;
+}
--- backend/springboot/shoppingmall/src/test/java/com/practice/shoppingmall/constant/ItemConstant.java
+++ backend/springboot/shoppingmall/src/test/java/com/practice/shoppingmall/constant/ItemConstant.java
@@ -1,4 +1,7 @@
 package com.practice.shoppingmall.constant;
 
 public class ItemConstant {
+    public static final String NAME = "포켓몬빵";
+    public static final int PRICE = 2000;
+    public static final int STOCK = 100;
 }
