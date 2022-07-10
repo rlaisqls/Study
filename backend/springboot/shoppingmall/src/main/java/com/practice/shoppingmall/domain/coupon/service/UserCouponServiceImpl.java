a3a3b8e15b231d6da25c367836f4eaa5f000cfae
rlaisqls <rlaisqls@gmail.com>
2022-07-10 15:53:18 +0900

    🐛 :: 파라미터 변수 변경

--- backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/domain/coupon/service/UserCouponServiceImpl.java
+++ backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/domain/coupon/service/UserCouponServiceImpl.java
@@ -1,19 +1,18 @@
 package com.practice.shoppingmall.domain.coupon.service;
 
-import com.practice.shoppingmall.domain.user.facade.UserFacade;
-import com.practice.shoppingmall.domain.coupon.presentation.dto.response.FindCouponGroupResponse;
 import com.practice.shoppingmall.domain.coupon.domain.Coupon;
-import com.practice.shoppingmall.domain.coupon.domain.repository.CouponRepository;
 import com.practice.shoppingmall.domain.coupon.domain.UserCoupon;
+import com.practice.shoppingmall.domain.coupon.domain.repository.CouponRepository;
+import com.practice.shoppingmall.domain.coupon.presentation.dto.response.FindCouponGroupResponse;
+import com.practice.shoppingmall.domain.order.exception.OrderNotFoundException;
 import com.practice.shoppingmall.domain.user.domain.User;
 import com.practice.shoppingmall.domain.user.domain.repository.UserRepository;
-import com.practice.shoppingmall.domain.order.exception.OrderNotFoundException;
+import com.practice.shoppingmall.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,10 +29,7 @@ public class UserCouponServiceImpl implements UserCouponService{
 
         User user = userFacade.nowUser();
 
-        List<Coupon> coupons =  user.getCoupons()
-                .stream()
-                .map(UserCoupon::getCoupon)
-                .collect(Collectors.toList());
+        List<UserCoupon> coupons = new ArrayList<>(user.getCoupons());
 
         return FindCouponGroupResponse.of(coupons);
     }
