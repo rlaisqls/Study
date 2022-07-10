2de4301ffe8e1c7702cbca9f0101657ea6b8c892
rlaisqls <rlaisqls@gmail.com>
2022-07-10 15:44:15 +0900

    🌧 :: expirationDate 추가

--- backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/domain/coupon/domain/UserCoupon.java
+++ backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/domain/coupon/domain/UserCoupon.java
@@ -16,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -38,5 +39,7 @@ public class UserCoupon {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "coupon_id")
     private Coupon coupon;
+
+    private LocalDateTime expirationDate;
 
 }
