641f653fc7bac34a33b6ad35794f9b5ec116d23c
rlaisqls <rlaisqls@gmail.com>
2022-07-11 19:22:04 +0900

    🐛 ::  HttpRequestMethodNotSupportedException 처리

--- backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/global/error/GlobalExceptionHandler.java
+++ backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/global/error/GlobalExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import javax.validation.ConstraintViolationException;
 import java.util.Arrays;
 
 @Slf4j
@@ -33,6 +32,13 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        String errorDescription = e.getMessage();
+        ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED, errorDescription);
+        return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
@@ -42,6 +48,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        System.out.println("e.getClass() = " + e.getClass());
+        Arrays.stream(e.getStackTrace()).forEach(System.out::println);
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         ErrorResponse response = ErrorResponse.of(errorCode, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
