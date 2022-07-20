061d7741b61181309b314b26bc1d162137b52228
rlaisqls <rlaisqls@gmail.com>
2022-07-20 11:12:36 +0900

    ⚡️ :: 메일 전송 실패 비즈니스에러

--- backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/global/util/JmsUtil.java
+++ backend/springboot/shoppingmall/src/main/java/com/practice/shoppingmall/global/util/JmsUtil.java
@@ -1,12 +1,12 @@
 package com.practice.shoppingmall.global.util;
 
+import com.practice.shoppingmall.domain.user.exception.MailSendFailException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
-import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 @Component
@@ -18,16 +18,21 @@ public class JmsUtil {
 
     private final JavaMailSender mailSender;
 
-    public void sendMassage(String email, String code) throws MessagingException {
+    public void sendMassage(String email, String code) {
 
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper(message, false, "UTF-8");
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, false, "UTF-8");
 
-        messageHelper.setTo(email);
-        messageHelper.setFrom(fromAddress);
-        messageHelper.setSubject("이메일 인증");
-        messageHelper.setText(code + "를 입력하세요");
+            messageHelper.setTo(email);
+            messageHelper.setFrom(fromAddress);
+            messageHelper.setSubject("이메일 인증");
+            messageHelper.setText(code + "를 입력하세요");
 
-        mailSender.send(message);
+            mailSender.send(message);
+        } catch (Exception e){
+            throw MailSendFailException.EXCEPTION;
+        }
+
     }
 }
