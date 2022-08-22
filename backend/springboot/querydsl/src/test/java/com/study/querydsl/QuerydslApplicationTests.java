232f9562540f4d8bb7526d0db2a04e4ba972b4b0
rlaisqls <rlaisqls@gmail.com>
2022-08-22 16:45:38 +0900

    📑 :: querydsl

--- backend/springboot/querydsl/src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ backend/springboot/querydsl/src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,13 @@
-package com.study.jpa;
+package com.study.querydsl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpaApplicationTests {
+class QuerydslApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
