d5585474b9fd317bd0df0a2a3943c92e9b516c0d
rlaisqls <rlaisqls@gmail.com>
2022-08-24 21:30:11 +0900

    📑 :: MemberDto

---
+++ backend/springboot/querydsl/src/main/java/com/study/querydsl/dto/MemberDto.java
@@ -0,0 +1,20 @@
+package com.study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@Getter
+public class MemberDto {
+    private String username;
+    private int age;
+
+    @QueryProjection
+    public MemberDto(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
