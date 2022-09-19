232f09852ed7bd1cbf68243a595ed37df76e2842
rlaisqls <rlaisqls@gmail.com>
2022-09-19 23:35:13 +0900

    🌟 :: 코틀린 연습

---
+++ backend/kotlin/kotlinPractice/src/main/kotlin/com/study/kotlinpractice/KotlinPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.study.kotlinpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinPracticeApplication>(*args)
+}
---
+++ backend/kotlin/kotlinPractice/src/test/kotlin/com/study/kotlinpractice/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.kotlinpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
